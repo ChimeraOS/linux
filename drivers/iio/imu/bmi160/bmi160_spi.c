9e8f4312b1867a240877bdda42354da42906c2b0
Denis Benato <benato.denis96@gmail.com>
2025-04-28 21:39:42 +0200

    iio: bmi160: suspend and resume triggering on relevant pm operations
    Prevent triggers from stop working after the device has entered sleep:
    use iio_device_suspend_triggering and iio_device_resume_triggering helpers.
    
    Signed-off-by: Denis Benato <benato.denis96@gmail.com>

--- drivers/iio/imu/bmi160/bmi160_spi.c
+++ drivers/iio/imu/bmi160/bmi160_spi.c
@@ -61,6 +61,7 @@ static struct spi_driver bmi160_spi_driver = {
 		.acpi_match_table	= bmi160_acpi_match,
 		.of_match_table		= bmi160_of_match,
 		.name			= "bmi160_spi",
+		.pm			= pm_ptr(&bmi160_core_pm_ops),
 	},
 };
 module_spi_driver(bmi160_spi_driver);
